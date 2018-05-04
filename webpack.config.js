3dac17828a12e14f30f295f29c2d774cb2e035cf
Muhannad Abdelrazek <muhnad.abdelrazek@gmail.com>
2018-05-04 17:21:51 +0300

    Eslint/fix indent (#40)
    * 2.0.1
    
    * Fix indant

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    entry: __dirname + '/src/a11y.js',
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/dist/',
-        filename: 'a11y.js',
-        library: 'a11yChecker',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }]
-    }
+  entry: __dirname + '/src/a11y.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist/',
+    filename: 'a11y.js',
+    library: 'a11yChecker',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: 'babel-loader'
+    }]
+  }
 };
